2a978566cce3fbb786a34feff709991cc4333f1a
Lautaro <laumol159@gmail.com>
2023-04-04 19:43:18 -0300

    primer commit

---
+++ appDate/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace appDate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.diatxt = new System.Windows.Forms.TextBox();
+            this.mestxt = new System.Windows.Forms.TextBox();
+            this.añotxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.fecha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // diatxt
+            // 
+            this.diatxt.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.diatxt.Location = new System.Drawing.Point(12, 72);
+            this.diatxt.MaxLength = 2;
+            this.diatxt.Name = "diatxt";
+            this.diatxt.Size = new System.Drawing.Size(66, 20);
+            this.diatxt.TabIndex = 0;
+            this.diatxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.diatxt_KeyPress);
+            // 
+            // mestxt
+            // 
+            this.mestxt.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mestxt.Location = new System.Drawing.Point(125, 72);
+            this.mestxt.MaxLength = 2;
+            this.mestxt.Name = "mestxt";
+            this.mestxt.Size = new System.Drawing.Size(64, 20);
+            this.mestxt.TabIndex = 1;
+            this.mestxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.diatxt_KeyPress);
+            // 
+            // añotxt
+            // 
+            this.añotxt.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.añotxt.Location = new System.Drawing.Point(252, 72);
+            this.añotxt.MaxLength = 4;
+            this.añotxt.Name = "añotxt";
+            this.añotxt.Size = new System.Drawing.Size(72, 20);
+            this.añotxt.TabIndex = 2;
+            this.añotxt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.diatxt_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese en orden dia fecha y mes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "DD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(137, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "MM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Black", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(269, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "AÑO";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(347, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Mostrar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fecha
+            // 
+            this.fecha.AutoSize = true;
+            this.fecha.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fecha.Location = new System.Drawing.Point(9, 130);
+            this.fecha.Name = "fecha";
+            this.fecha.Size = new System.Drawing.Size(0, 19);
+            this.fecha.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 171);
+            this.Controls.Add(this.fecha);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.añotxt);
+            this.Controls.Add(this.mestxt);
+            this.Controls.Add(this.diatxt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Fecha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox diatxt;
+        private System.Windows.Forms.TextBox mestxt;
+        private System.Windows.Forms.TextBox añotxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label fecha;
+    }
+}
+
---
+++ appDate/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace appDate
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        
+
+        private void diatxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <=47 ) || (e.KeyChar >= 58 && e.KeyChar <= 255)) 
+            {
+                e.Handled = true;
+                return;
+            }
+        }
+        string[] meses = new string[12] { "Enero", "Febrero", "Marzo", "Abril" , "Mayo" , "junio" , "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+        bool bisiesto = true;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(diatxt.Text) > 31) { MessageBox.Show("ningun mes tiene mas de 31 dias");}
+            else { 
+
+            if (mestxt.Text == "" || diatxt.Text == "" || añotxt.Text == "" ) { MessageBox.Show("llene TODOS los campos"); }
+            else { 
+            int año = Convert.ToInt32(añotxt.Text);
+            int messel = Convert.ToInt32(mestxt.Text) - 1;
+            if (messel + 1 <= 0 || messel + 1 >= 13) { MessageBox.Show("los meses del año deben insertarse con un numero del 1 al 12"); }
+            else { 
+                if (año % 400 == 0 || (año % 4 == 0 && año % 100 != 0) ) { bisiesto = true; }
+                    else {  bisiesto = false;}
+                        if (!bisiesto && (Convert.ToInt32(diatxt.Text) >= 29) && (Convert.ToInt32(mestxt.Text) - 1 == 1 )) 
+                                { MessageBox.Show("no es un año bisiesto por lo tanto febrero no tiene 29 dias o mas"); }
+                        else { if (Convert.ToInt32(diatxt.Text) > 30 && (Convert.ToInt32(mestxt.Text) - 1 == 3 || Convert.ToInt32(mestxt.Text) - 1 == 5 || Convert.ToInt32(mestxt.Text) - 1 == 8
+                                || Convert.ToInt32(mestxt.Text) - 1 == 10 || (messel == 1 && Convert.ToInt32(diatxt.Text) >= 28) ) ) { MessageBox.Show("este mes tiene menos de 31 dias"); } 
+                        else { fecha.Text = ("La fecha es:  " + diatxt.Text + " del mes:  " + meses[messel] + " del año:  " + añotxt.Text ); }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
