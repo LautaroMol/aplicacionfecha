77dd5ff55aab10937f7210911a813e9ad8bddde4
Lautaro <laumol159@gmail.com>
2023-04-10 20:14:23 -0300

    correcciones

--- appDate/Form1.cs
+++ appDate/Form1.cs
@@ -22,45 +22,61 @@ namespace appDate
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
+
         }
 
-        
+
 
         private void diatxt_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if ((e.KeyChar >= 32 && e.KeyChar <=47 ) || (e.KeyChar >= 58 && e.KeyChar <= 255)) 
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
             {
                 e.Handled = true;
                 return;
             }
         }
-        string[] meses = new string[12] { "Enero", "Febrero", "Marzo", "Abril" , "Mayo" , "junio" , "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"};
+        string[] meses = new string[12] { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre" };
         bool bisiesto = true;
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (Convert.ToInt32(diatxt.Text) > 31) { MessageBox.Show("ningun mes tiene mas de 31 dias");}
-            else { 
+            if (mestxt.Text == "" || diatxt.Text == "" || añotxt.Text == "") { MessageBox.Show("llene TODOS los campos"); }
+            else
+            {
 
-            if (mestxt.Text == "" || diatxt.Text == "" || añotxt.Text == "" ) { MessageBox.Show("llene TODOS los campos"); }
-            else { 
-            int año = Convert.ToInt32(añotxt.Text);
-            int messel = Convert.ToInt32(mestxt.Text) - 1;
-            if (messel + 1 <= 0 || messel + 1 >= 13) { MessageBox.Show("los meses del año deben insertarse con un numero del 1 al 12"); }
-            else { 
-                if (año % 400 == 0 || (año % 4 == 0 && año % 100 != 0) ) { bisiesto = true; }
-                    else {  bisiesto = false;}
-                        if (!bisiesto && (Convert.ToInt32(diatxt.Text) >= 29) && (Convert.ToInt32(mestxt.Text) - 1 == 1 )) 
-                                { MessageBox.Show("no es un año bisiesto por lo tanto febrero no tiene 29 dias o mas"); }
-                        else { if (Convert.ToInt32(diatxt.Text) > 30 && (Convert.ToInt32(mestxt.Text) - 1 == 3 || Convert.ToInt32(mestxt.Text) - 1 == 5 || Convert.ToInt32(mestxt.Text) - 1 == 8
-                                || Convert.ToInt32(mestxt.Text) - 1 == 10 || (messel == 1 && Convert.ToInt32(diatxt.Text) >= 28) ) ) { MessageBox.Show("este mes tiene menos de 31 dias"); } 
-                        else { fecha.Text = ("La fecha es:  " + diatxt.Text + " del mes:  " + meses[messel] + " del año:  " + añotxt.Text ); }
+                if (Convert.ToInt32(diatxt.Text) > 31) { MessageBox.Show("ningun mes tiene mas de 31 dias"); }
+                else
+                {
+                    int año = Convert.ToInt32(añotxt.Text);
+                    int messel = Convert.ToInt32(mestxt.Text) - 1;
+                    int dia = Convert.ToInt32(diatxt.Text);
+                    bool corto = false;
+                    bool cambioMes = false;
+                    if (messel + 1 == 4 || messel + 1 == 6 || messel == 9 || messel + 1 == 11) { corto = true; }
+
+                    if (messel + 1 <= 0 || messel + 1 >= 13) { MessageBox.Show("los meses del año deben insertarse con un numero del 1 al 12"); }
+                    else
+                    {
+                        if (año % 400 == 0 || (año % 4 == 0 && año % 100 != 0)) { bisiesto = true; }
+                        else { bisiesto = false; }
+                        if (corto && messel != 1 && dia == 30) { messel += 1; dia = 1; cambioMes = true; }
+                        else
+                        {
+                            if (dia == 31 && messel != 1)
+                            {
+                                if (messel == 11) { año += 1; messel = 0; dia = 1; cambioMes = true; }
+                                else { messel += 1; dia = 1; cambioMes = true; }
+                            }
+                            if (dia == 29 && messel == 1 && bisiesto) { messel += 1; dia = 1; cambioMes = true; }
+                            else { if (dia == 28 && messel == 1 && !bisiesto) { messel += 1; dia = 1; cambioMes = true; } }
+                            if (!cambioMes) { dia += 1; }
+
+                            MessageBox.Show("mañana sera: " + dia + " del mes: " + meses[messel] + " del año: " + año);
+
                         }
                     }
                 }
             }
         }
     }
-
 }
